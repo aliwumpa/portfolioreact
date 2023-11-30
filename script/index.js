5592c7830253e22fd2ac23e031f43249b5b349f5
DESKTOP-8OPSHCT\User <aliwumpa@gmail.com>
2023-11-30 10:26:35 +0700

    improvement portofolio ( 4 ) : -animation of section wrapper title -separate content and header title -change gradient background color

--- script/index.js
+++ script/index.js
@@ -34,100 +34,101 @@ const toogleDisplayFunction = () => {
 }
 
 const fetchDisplayFunction = () => {
-    $('.display__container--outer').each((index, el) => {
-        const htmlAttr = $(el).data('html');
-        
-        if(htmlAttr !== '' && htmlAttr !== null && htmlAttr !== undefined) {
-            $.get(`./html/${htmlAttr}.html`, (data) => {
-                // Parse the HTML content
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(data, 'text/html');
-                
-                // Get all elements and their contents
-                const elements = doc.body.getElementsByTagName('*');
-                const headElements = doc.head.getElementsByTagName('*');
-                
-                // Create an HTML string with highlighting
-                let htmlString = '';
-                let cssString = '';
-                let scriptString = '';
-                let formattedScriptInnerHtml = '';
-                let formattedCSSInnerHTML = '';
-
-                // Result rendered to result container
-                $(el).siblings(resultContainer).find(resultRenderContainer).html(data);
-
-                if(headElements.length) {
-                    for (const head of headElements) {
-
-                        const tagName = head.tagName.toLowerCase();
-                        const highlightedElement = `<span style="color: #a7925a;">${tagName}</span>`;
-                        const cssInnerHtml = head.innerHTML;
-
-                        if (tagName === 'style') {
-                            for (let i = 0; i < cssInnerHtml.length; i++) {
-                                const char = cssInnerHtml[i];
-                                formattedCSSInnerHTML += char;
-                
-                                if (char === ';' || char === '{' || char === '}') {
-                                    formattedCSSInnerHTML += '<br />';
+    if($('.display__container--outer').length) {
+        $('.display__container--outer').each((index, el) => {
+            const htmlAttr = $(el).data('html');
+            
+            if(htmlAttr !== '' && htmlAttr !== null && htmlAttr !== undefined) {
+                $.get(`./html/${htmlAttr}.html`, (data) => {
+                    // Parse the HTML content
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(data, 'text/html');
+                    
+                    // Get all elements and their contents
+                    const elements = doc.body.getElementsByTagName('*');
+                    const headElements = doc.head.getElementsByTagName('*');
+                    
+                    // Create an HTML string with highlighting
+                    let htmlString = '';
+                    let cssString = '';
+                    let scriptString = '';
+                    let formattedScriptInnerHtml = '';
+                    let formattedCSSInnerHTML = '';
+    
+                    // Result rendered to result container
+                    $(el).siblings(resultContainer).find(resultRenderContainer).html(data);
+    
+                    if(headElements.length) {
+                        for (const head of headElements) {
+    
+                            const tagName = head.tagName.toLowerCase();
+                            const highlightedElement = `<span style="color: #a7925a;">${tagName}</span>`;
+                            const cssInnerHtml = head.innerHTML;
+    
+                            if (tagName === 'style') {
+                                for (let i = 0; i < cssInnerHtml.length; i++) {
+                                    const char = cssInnerHtml[i];
+                                    formattedCSSInnerHTML += char;
+                    
+                                    if (char === ';' || char === '{' || char === '}') {
+                                        formattedCSSInnerHTML += '<br />';
+                                    }
                                 }
+                    
+                                cssString += `<${highlightedElement}><br /><br />${formattedCSSInnerHTML}<br /><br />&lt/${highlightedElement}>`;
                             }
-                
-                            cssString += `<${highlightedElement}><br /><br />${formattedCSSInnerHTML}<br /><br />&lt/${highlightedElement}>`;
                         }
                     }
-                }
-
-                if(elements.length) {
-                    for (const element of elements) {
-                        const tagName = element.tagName.toLowerCase();
-                        const isSelfClosing = ['img', 'br'].includes(tagName);
+    
+                    if(elements.length) {
+                        for (const element of elements) {
+                            const tagName = element.tagName.toLowerCase();
+                            const isSelfClosing = ['img', 'br'].includes(tagName);
+                        
+                            const highlightedElement = `<span style="color: #a7925a;">${tagName}</span>`;
+                            const attributes = Array.from(element.attributes).map(attr => {
+                                return `<span style="color:#e6e600;">${attr.name}</span>=${attr.value}`;
+                            }).join(' ');
+                            const scriptInnerHtml = element.innerHTML;
+                            if (tagName === 'script') {            
+                                for (let i = 0; i < scriptInnerHtml.length; i++) {
+                                    const char = scriptInnerHtml[i];
+                                    formattedScriptInnerHtml += char;
                     
-                        const highlightedElement = `<span style="color: #a7925a;">${tagName}</span>`;
-                        const attributes = Array.from(element.attributes).map(attr => {
-                            return `<span style="color:#e6e600;">${attr.name}</span>=${attr.value}`;
-                        }).join(' ');
-                        const scriptInnerHtml = element.innerHTML;
-                        if (tagName === 'script') {            
-                            for (let i = 0; i < scriptInnerHtml.length; i++) {
-                                const char = scriptInnerHtml[i];
-                                formattedScriptInnerHtml += char;
-                
-                                if (char === ';') {
-                                    formattedScriptInnerHtml += '<br />';
+                                    if (char === ';') {
+                                        formattedScriptInnerHtml += '<br />';
+                                    }
                                 }
-                            }
-                
-                            scriptString += `<${highlightedElement} ${attributes}><br /><br />${formattedScriptInnerHtml}<br /><br />&lt/${highlightedElement}>`;
-    
-                        } else {
-                            if (isSelfClosing) {
-                                htmlString += `<${highlightedElement} ${attributes} />`;
-                            } else {
-                                htmlString += `<${highlightedElement} ${attributes}>${element.innerHTML}&lt/${highlightedElement}>`;
-                            }
-                        }    
-                    }
-                }
-
-                // Write the HTML string to a specific container
-                if(htmlString !== '' && cssString !== '' && scriptString !=='') {
-                    $(el).find(htmlBox).eq(0).html(htmlString);
-                    $(el).find(htmlBox).eq(1).html(cssString);
-                    $(el).find(htmlBox).eq(2).html(scriptString);
-                } else {
-                    $(el).find(htmlBox).eq(0).html(htmlString);
-                    $(el).find(htmlBox).eq(1).html(scriptString);                  
-                }
-            })
-            .fail(() => {
-                console.error('Error loading the file');
-            });
-        }
+                    
+                                scriptString += `<${highlightedElement} ${attributes}><br /><br />${formattedScriptInnerHtml}<br /><br />&lt/${highlightedElement}>`;
         
-    })
+                            } else {
+                                if (isSelfClosing) {
+                                    htmlString += `<${highlightedElement} ${attributes} />`;
+                                } else {
+                                    htmlString += `<${highlightedElement} ${attributes}>${element.innerHTML}&lt/${highlightedElement}>`;
+                                }
+                            }    
+                        }
+                    }
     
+                    // Write the HTML string to a specific container
+                    if(htmlString !== '' && cssString !== '' && scriptString !=='') {
+                        $(el).find(htmlBox).eq(0).html(htmlString);
+                        $(el).find(htmlBox).eq(1).html(cssString);
+                        $(el).find(htmlBox).eq(2).html(scriptString);
+                    } else {
+                        $(el).find(htmlBox).eq(0).html(htmlString);
+                        $(el).find(htmlBox).eq(1).html(scriptString);                  
+                    }
+                })
+                .fail(() => {
+                    console.error('Error loading the file');
+                });
+            }
+            
+        })   
+    }
 }
 
 const toggleAccordionFunction = () => {
@@ -142,9 +143,17 @@ const toggleAccordionFunction = () => {
     });
 }
 
+const notesFunction = () => {
+    if($('.see-notes--button').length) {
+        $('.see-notes--button').click(() => {
+            $('.section__wrapper--title').fadeOut();
+            $('#notes__section').show();
+        });
+    }
+}
+
 //invoke all function
 fetchDisplayFunction();
 toogleDisplayFunction();
 toggleAccordionFunction();
-
-
+notesFunction();
